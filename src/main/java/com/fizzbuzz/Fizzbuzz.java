b35f8e003a3b163e9c8609c3b926b83df72903bf
christianc2000 <84164905+christianc2000@users.noreply.github.com>
2025-05-08 09:33:10 -0400

    task: fizzbuzz v2

--- src/main/java/com/fizzbuzz/Fizzbuzz.java
+++ src/main/java/com/fizzbuzz/Fizzbuzz.java
@@ -3,15 +3,9 @@ package com.fizzbuzz;
 public class Fizzbuzz {
 
     public String isMultiple(int num) {
-        if (num % 15 == 0) {
-            return "FizzBuzz";
-        }
-        if (num % 3 == 0) {
-            return "Fizz";
-        }
-        if (num % 5 == 0) {
-            return "Buzz";
-        }
+        if (num % 15 == 0) return "FizzBuzz";
+        if (num % 3 == 0) return "Fizz";
+        if (num % 5 == 0) return "Buzz";
         return String.valueOf(num);
     }
 
